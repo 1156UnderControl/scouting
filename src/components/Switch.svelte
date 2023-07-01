77d958157bd19d5d1cbac9652359a614916205d8
Lucca J. Becker <81322668+ljb6@users.noreply.github.com>
2023-07-01 16:25:58 -0300

    Update Switch.svelte
    Troquei o CSS normal pelo Tailwind

--- src/components/Switch.svelte
+++ src/components/Switch.svelte
@@ -2,64 +2,7 @@
   export let checked = false;
 </script>
 
-<style>
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-    
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    border-radius: 34px;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    border-radius: 50%;
-  }
-
-  input:checked + .slider {
-    background-color: #0061a6;
-  }
-
-  input:checked + .slider {
-    box-shadow: 0 0 1px #2196f3;
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-
-</style>
-
-<label class="switch">
-  <input type="checkbox" bind:checked/>
-  <span class="slider"/>
+<label class="relative inline-flex items-center mb-4 cursor-pointer">
+  <input type="checkbox" value="" class="sr-only peer" bind:checked> 
+  <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-900 peer-checked:bg-blue-800"></div>
 </label>
