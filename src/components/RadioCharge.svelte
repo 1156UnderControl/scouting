d959a280cd58271978b27a37a9569fc772c575fe
Lucca J. Becker <81322668+ljb6@users.noreply.github.com>
2023-07-24 18:28:58 -0300

    Create RadioCharge.svelte
    Novo radio para opções da charge, ainda existe o antigo para usarmos com a API do TBA

---
+++ src/components/RadioCharge.svelte
@@ -0,0 +1,18 @@
+<script>
+	let scoops = 1;
+</script>
+
+<ul class="flex items-center rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600">
+    <label class="p-3">
+        <input type="radio" bind:group={scoops} value={1}/>
+        Engaged
+    </label>
+    <label class="p-3">
+        <input type="radio" bind:group={scoops} value={2}/>
+        Docked
+    </label>
+    <label class="p-3">
+        <input type="radio" bind:group={scoops} value={3}/>
+        Park
+    </label>
+</ul>
